80bead27d9d93488b7f6154d989b891e5b4ebc08
omersb <omersaidbuldukk@gmail.com>
2023-01-02 22:04:38 +0300

    faker update

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=25, unique=True)
+    name = models.CharField(max_length=25)
 
     def __str__(self):
         return self.name
