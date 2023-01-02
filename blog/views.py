11bef51589409183d948e06c52d5a0cecd46da86
omersb <omersaidbuldukk@gmail.com>
2023-01-02 21:40:06 +0300

    added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Category
 
-# Create your views here.
+
+class CategoryView(ModelViewSet):  # crud
+    queryset = Category.objects.all()
+    serializer_class = None
