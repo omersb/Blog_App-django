bbceb648ff18a0c1c565bd11ceff5ad1dbfd8f81
omersb <omersaidbuldukk@gmail.com>
2023-01-02 21:57:26 +0300

    update

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .models import Category
+
+from .models import Category, Blog
+from .serializers import CategorySerializer, BlogSerializer
 
 
 class CategoryView(ModelViewSet):  # crud
     queryset = Category.objects.all()
-    serializer_class = None
+    serializer_class = CategorySerializer
+
+
+class BlogView(ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
